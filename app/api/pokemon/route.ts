54d6562bc56b322c6b0a17797fdff5d8dced006e
mmontfort <m.montfort@betterfly-tourism.com>
2024-04-23 12:21:24 +0200

    adding routes building in pnpm run build + small fix

--- app/api/pokemon/route.ts
+++ app/api/pokemon/route.ts
@@ -1,6 +1,5 @@
+import { getAllPokemons } from "@/lib/utils/pokemon";
 import { NextRequest, NextResponse } from "next/server";
-
-import { getFullPokemon } from "@/pokemon";
 
 export const dynamic = "force-dynamic";
 
@@ -8,5 +7,5 @@ export async function GET(req: NextRequest) {
   const url = new URL(req.url);
   const q = url.searchParams.get("q") ?? "";
   const limit = url.searchParams.get("limit") ?? 10;
-  return NextResponse.json(await getFullPokemon(+limit, q));
+  return NextResponse.json(await getAllPokemons(+limit, q));
 }
